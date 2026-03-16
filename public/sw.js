eee0fc022fff9a7bbc385c4b3934514f473bff97
Vineet Yadav <vineetyadav9991@gmail.com>
2026-03-16 21:13:11 +0530

    Update service worker cache and assets
    Updated cache version and added new assets for caching.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'studytrack-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'studytrack-v3';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/favicon.ico'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
